c9687f088a1e9b424c4358bf2548d6fbd06f780e
aidanOHalloran <amohalloran@crimson.ua.edu>
2023-06-21 16:48:29 -0500

    added icons to gameCards

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -9,7 +9,7 @@ const GameGrid = () => {
     <>
       {error && <Text textColor={'red'} fontSize='xl'>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg:3, xl:5 }} padding={10} spacing={10}>
-        {games.map((game) => (
+        {games.map((game) => (  //map through the games array retrieved via useGames hook
           <GameCard key={game.id} game={game}/>
         ))}
       </SimpleGrid>
